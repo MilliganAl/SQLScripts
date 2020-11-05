d66fdbec0afe71fea57c89cb04005e7ad4e9cbca
MilliganAl <51169129+MilliganAl@users.noreply.github.com>
2020-11-05 10:42:19 -0600

    Adding initial files
    Adding initial files for first commit.

---
+++ SearchStoredForProcedure.sql
@@ -0,0 +1,5 @@
+SELECT [Name],
+[schema] = OBJECT_SCHEMA_NAME([object_id])
+FROM sys.procedures
+WHERE OBJECT_DEFINITION(OBJECT_ID) LIKE '%Employees%'
+order by 1
---
+++ SingleUserModesql.sql
@@ -0,0 +1,31 @@
+/*
+Promotes will be done before promote after checking the databases have been backed up
+Remote into Production Server and stop IIS
+Put the database into Single User mode
+Run the ALTER statements
+Promote the Applications
+Set the database to Multi-User mode
+Start IIS
+*/
+
+USE master;
+GO
+ALTER DATABASE ][dB_Name]
+SET SINGLE_USER
+WITH ROLLBACK AFTER 60;
+GO
+
+USE [dB_Name]
+GO
+
+/*
+    ALTER HERE
+*/
+
+GO
+
+USE master
+GO
+ALTER DATABASE [dB_Name]
+SET MULTI_USER;
+GO
